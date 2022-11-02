4b25c3a1ae7f65869a25423263b3e110ae25c160
meteor314 <meteor3141592@gmail.com>
2022-11-02 22:48:29 +0100

    replace mdb with tailwind and recreate all client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
-import ForgetPassword from "./components/ForgetPassword";
-import Home from "./components/Home";
-import User from "./components/User";
-import Setting from "./components/Setting";
+import Register from './components/Register';
+import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// css
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+
+
 
 function App() {
-  //
-
   return (
-    <BrowserRouter>
-       <Routes>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forget-password" element={<ForgetPassword />} />
-        <Route path="/" element={<Home />} />
-        <Route exact path="/:username" element={<User />} />
-        <Route path="/:username/settings" elment={<Setting />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
